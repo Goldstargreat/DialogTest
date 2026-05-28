608ffd377958e02c1a069285db4e20f7c3dbd9c7
goldstargreat <goldstargreat@gmail.com>
2026-05-28 15:08:01 +0900

    checkbox 항목 대화상자

--- app/src/main/java/kr/ac/kopo/dialogtest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/dialogtest/MainActivity.java
@@ -15,7 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity
 {
     Button btn;
-    String[] itemArr = {"떡볶이, 튀김, 순대"};
+    boolean[] checkArr = {false, false, false};
+    String[] itemArr = {"떡볶이", "튀김", "순대"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +38,40 @@ public class MainActivity extends AppCompatActivity
             AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
             dialog.setTitle("버튼 라벨 변경");
             dialog.setIcon(R.drawable.female);
-            dialog.setItems(itemArr, new DialogInterface.OnClickListener()
-            {
+            // 체크 박스가 선택된 항목이 모두 버튼의 텍스트로 보이게끔
+            // 체크 박스 항목으로 구성된 대화상자
+            dialog.setMultiChoiceItems(itemArr, checkArr, new DialogInterface.OnMultiChoiceClickListener() {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    btn.setText(itemArr[which]);
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                    btn.setText("");
+                    if(isChecked)
+                    {
+                        checkArr[which] = isChecked;
+                    }
+                        for(int i = 0; i < checkArr.length; i++)
+                        {
+                            if (checkArr[i]) {
+                                btn.append(itemArr[i] + " :: ");
+                            }
+                        }
                 }
             });
+            //  라디오 버튼 항목으로 구성된 대화상자
+//            dialog.setSingleChoiceItems(itemArr, 0, new DialogInterface.OnClickListener()
+//            {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    btn.setText(itemArr[which]);
+//                }
+//            });
+//            항목으로 구성된 다이얼로그
+//            dialog.setItems(itemArr, new DialogInterface.OnClickListener()
+//            {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    btn.setText(itemArr[which]);
+//                }
+//            });
             dialog.setPositiveButton("닫기", null);
             dialog.show();
         }
